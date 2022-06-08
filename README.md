66ed73ed335324489ff277513a02dcb8c33ab2e0
JlSanchez05 <jlsanchez2405@gmail.com>
2022-06-08 15:28:07 -0400

    added more final changes

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Home-Work-Challange-03
+
+In this Home Work challange our task is to create a javascipt
+application that enables employees to generate a random password.
+
+The password length must be between 8 to 128 characters
+The Users have the options to choose weather they want the following options
+1: Upper case letters
+2: Lower case letters
+3: Numbers
+4: Special characters
+The Program is also built to enable users who dont pick
+from the following criteria to try again until they
+decide to follow the instructions.
--- script.js
+++ script.js
@@ -10,10 +10,11 @@ var userChoices
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 // Upper Case array
 var blankUpper = []
+//functions that converts lower to upper case
 var toUpper = function (x){
   return x.toUpperCase()
 }
-
+//Declaring lower case array to switch to upper
 upperCase = lowerCase.map(toUpper)
 //const variables because they will never change value
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -107,3 +108,4 @@ function generatePassword() {
   console.log("Your Pasword is: " + password)
   return password
 }
+// Dev Juan Sanchez
