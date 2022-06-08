6909cf7c7a7514c9187ad42c6f08e7323bf061f0
JlSanchez05 <jlsanchez2405@gmail.com>
2022-06-08 15:28:42 -0400

    fogot to save something in script file

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ upperCase = lowerCase.map(toUpper)
 //const variables because they will never change value
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
-
 var generateBtn = document.querySelector("#generate")
 
 // Write password to the #password input
